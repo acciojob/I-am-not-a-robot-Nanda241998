7459eb081f2cde6ae7523971d0cc7ce601ec67a8
Nanda241998 <117671873+Nanda241998@users.noreply.github.com>
2023-08-07 22:30:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-//your code here
-document.getElementByTagName('Img').addEventListener('click', function(){
-	alert(this);
-});
+var noDrumBtn = document.querySelectorAll("img").length;
+console.log('No of Buttons :' +noDrumBtn);
